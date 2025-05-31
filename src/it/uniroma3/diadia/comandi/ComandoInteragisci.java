eb038771be72c0968735aeca476cfe4df7c07164
GiuseppeSemeraro <semeraro.giuseppe1512@gmail.com>
2025-05-31 14:16:08 +0200

    comandi

---
+++ src/it/uniroma3/diadia/comandi/ComandoInteragisci.java
@@ -0,0 +1,26 @@
+package it.uniroma3.diadia.comandi;
+
+import it.uniroma3.diadia.IO;
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.personaggi.AbstractPersonaggio;
+public class ComandoInteragisci extends AbstractComando{
+	private static final String MESSAGGIO_CON_CHI="Con chi interagisco?";
+	private String messaggio;
+	private IO io;
+	@Override
+	public void esgui(Partita partita) {
+		AbstractPersonaggio perosnaggio;
+		personaggio=partita.getStanzaCorrente().getPersonaggio();
+		if(personaggio!=null) {
+			this.messaggio=personaggio.agisci(partita);
+			io.mostraMessaggio(this.messaggio);
+		}else io.mostraMessaggio(MESSAGGIO_CON_CHI);
+	}
+	public String getMessaggio() {
+		return this.messaggio;
+	}
+	@Override
+	public void setIo(IO io) {
+		this.io=io;
+	}
+}
--- test/it/uniroma3/diadia/ambienti/StanzaMagicaTest.java
+++ test/it/uniroma3/diadia/ambienti/StanzaMagicaTest.java
@@ -1,6 +1,5 @@
 package it.uniroma3.diadia.ambienti;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
